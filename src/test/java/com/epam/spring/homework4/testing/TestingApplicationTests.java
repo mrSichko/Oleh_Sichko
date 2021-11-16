b484df18c9727f399fe45f73800d0b4807d21f63
Oleh Sichko <mrSichko@gmail.com>
2021-11-16 11:52:45 +0200

    add spring boot

---
+++ src/test/java/com/epam/spring/homework4/testing/TestingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.epam.spring.homework4.testing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
